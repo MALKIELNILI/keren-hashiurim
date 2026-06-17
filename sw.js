d9e195f7fee8573d3d73d0d1100a8b78aa36ae52
MALKIELNILI <malkielnili@gmail.com>
2026-06-17 22:07:21 +0300

    עדכון cache v2 לניקוי service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'keren-v1';
+const CACHE = 'keren-v2';
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(['./index.html','./manifest.json','./icon.svg']))
 ));
